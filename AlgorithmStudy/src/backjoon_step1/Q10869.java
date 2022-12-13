79a573634cae624a88b65aec0ebdf069fe7ebbdf
choieuihyun <amammmaa1@naver.com>
2022-12-13 21:57:45 +0900

    step 1 진짜 완료

---
+++ AlgorithmStudy/src/backjoon_step1/Q10869.java
@@ -0,0 +1,22 @@
+package backjoon_step1;
+
+import java.util.Scanner;
+
+public class Q10869 { // Main으로 해야 백준 실행됨.
+	
+	public static void main(String [] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.print(a%b);
+		
+	}
+	
+
+}
---
+++ AlgorithmStudy/src/backjoon_step1/Q3003.java
@@ -0,0 +1,24 @@
+package backjoon_step1;
+
+import java.util.Scanner;
+
+public class Q3003 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int[] chess = {1,1,2,2,2,8};
+		int[] donk = new int[6];
+		
+		for(int i = 0; i < chess.length; i++) {
+			donk[i] = scanner.nextInt();
+
+		}
+		
+		for(int i = 0; i < chess.length; i++) {
+			System.out.print(chess[i] - donk[i] + " ");
+		}
+		
+	}
+}
