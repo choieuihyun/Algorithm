2e9c2529e81fff001051af18f9d1ba7aae3202f7
choieuihyun <amammmaa1@naver.com>
2022-12-13 21:58:14 +0900

    step 3 완료

---
+++ AlgorithmStudy/src/baekjoon_step3/Q10950.java
@@ -0,0 +1,22 @@
+package baekjoon_step3;
+
+import java.util.Scanner;
+
+public class Q10950 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+
+		for(int i = 0; i < num; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println(a+b);
+		}
+
+
+	}
+
+}
---
+++ AlgorithmStudy/src/baekjoon_step3/Q11021.java
@@ -0,0 +1,22 @@
+package baekjoon_step3;
+
+import java.util.Scanner;
+
+public class Q11021 {
+
+	public static void main(String[] args)  {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		for(int i = 1; i < num+1; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			System.out.println("Case #" + i + ": " + (a+b));
+		}
+		
+	}
+
+}
