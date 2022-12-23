68d2810b465bf988c7fcd7773f60ce0a4ad148ae
choieuihyun <amammmaa1@naver.com>
2022-12-23 21:00:36 +0900

    step7 진행중

---
+++ AlgorithmStudy/src/baekjoon_step7/Q1712.java
@@ -0,0 +1,41 @@
+package baekjoon_step7;
+
+import java.util.Scanner;
+
+public class Q1712 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		
+		if(c-b <= 0) {
+			System.out.print(-1);
+		} else
+			System.out.print(a/(c-b)+1);
+		
+		
+		/* 21억번 반복하는 코드
+		 * 
+		 * long n = 0;
+		 * 
+		 * int count = 0;
+		 * 
+		 * while((n*c) <= a+(b*n)) {
+		 * 
+		 * n++;
+		 * 
+		 * }
+		 * 
+		 * if(b >= c) { n = -1; }
+		 * 
+		 * System.out.print(n);
+		 */
+	
+
+	}
+
+}
