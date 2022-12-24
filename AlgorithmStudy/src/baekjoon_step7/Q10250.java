06e8eaa61aedc4a0108ba3e5ac0d1e04f07f97cf
choieuihyun <amammmaa1@naver.com>
2022-12-24 20:01:04 +0900

    step7 진행중

---
+++ AlgorithmStudy/src/baekjoon_step7/Q10250.java
@@ -0,0 +1,44 @@
+package baekjoon_step7;
+
+import java.util.Scanner;
+
+public class Q10250 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int A = 0;
+		int B = 0;
+		
+		for(int i = 0; i < num; i++) {
+			
+			int h = scanner.nextInt();
+			int w = scanner.nextInt();
+			int n = scanner.nextInt();
+			
+			
+			if(n % h != 0) {
+				A = (n % h) * 100; // 사람이 들어간 층
+			} else {
+				A = h * 100;
+			}
+				
+			
+			
+			if(n % h != 0)
+				B = n / h + 1;
+			else
+				B = n / h;
+		}
+		
+		
+		int AABB = A+B;
+		
+		System.out.print(AABB);
+		
+		
+	}
+}
+
+
