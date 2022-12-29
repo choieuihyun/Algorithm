86056f2600dc82889716fbf6ed5fbd5ee1a9a358
choieuihyun <amammmaa1@naver.com>
2022-12-29 19:08:13 +0900

    step7 완료

--- AlgorithmStudy/src/baekjoon_step7/Q10250.java
+++ AlgorithmStudy/src/baekjoon_step7/Q10250.java
@@ -5,38 +5,24 @@ import java.util.Scanner;
 public class Q10250 { // ¿Ö Ä¿¹ÔÀÌ ¾ÈµÇÂ¡
 
 	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		int A = 0;
-		int B = 0;
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+
 		for(int i = 0; i < num; i++) {
 			
-			int h = scanner.nextInt();
-			int w = scanner.nextInt();
-			int n = scanner.nextInt();
-			
+			int h = scanner.nextInt(); // Ãþ ¼ö
+			int w = scanner.nextInt(); // È£ ¼ö
+			int n = scanner.nextInt(); // n¹øÂ° ¼Õ´Ô
 			
 			if(n % h != 0) {
-				A = (n % h) * 100; // »ç¶÷ÀÌ µé¾î°£ Ãþ
+				System.out.println(((n%h) * 100) + ((n/h) + 1));
 			} else {
-				A = h * 100;
+				System.out.println((h*100) + (n/h));
 			}
-				
-			
-			
-			if(n % h != 0)
-				B = n / h + 1;
-			else
-				B = n / h;
+					
 		}
-		
-		
-		int AABB = A+B;
-		
-		System.out.print(AABB);
-		
 		
 	}
 }
