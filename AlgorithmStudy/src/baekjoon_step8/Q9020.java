303a4e3705af443a745d0e9da18e1e21b1b6fbb9
choieuihyun <amammmaa1@naver.com>
2023-01-02 17:40:03 +0900

    step8 진행중

---
+++ AlgorithmStudy/src/baekjoon_step8/Q9020.java
@@ -0,0 +1,55 @@
+package baekjoon_step8;
+
+import java.util.Scanner;
+
+public class Q9020 {
+	
+	static boolean[] prime = new boolean[10001];
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+	
+		get_prime();
+			
+		int t = scanner.nextInt();
+			
+		for(int i = 0; i < t; i++) {
+			
+			int s = scanner.nextInt();
+			int a = s/2;
+			int b = s/2;
+			
+			while(true) {
+				if(prime[a] == false && prime[b] == false) {
+					System.out.print(a + " " + b + "\n");
+					break;
+				}
+				a--;
+				b++;
+			}
+		}
+			
+}
+			
+	public static void get_prime() {
+		
+
+		prime[0] = prime[1] = true;
+		
+		for(int i = 2; i < prime.length; i++) {
+			if(prime[i])
+				continue;
+			
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+			
+			
+		}
+		
+
+		
+	}
+
+}
