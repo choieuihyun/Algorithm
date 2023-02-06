580e5fd1a89e6fd4027583df273fbfaa9693673f
choieuihyun <amammmaa1@naver.com>
2023-02-06 23:53:59 +0900

    step12 진행중

--- AlgorithmStudy/src/baekjoon_step12/Q7568.java
+++ AlgorithmStudy/src/baekjoon_step12/Q7568.java
@@ -9,30 +9,31 @@ public class Q7568 {
 		Scanner scanner = new Scanner(System.in);
 		
 		int num = scanner.nextInt();
-		
-		int array[][] = new int[num][2];
-		int result[] = new int[num];
-		
+		int [][] array = new int [num][2];
+		int [] resultArray = new int[num];
 		
 		for(int i = 0; i < num; i++) {
 			array[i][0] = scanner.nextInt();
 			array[i][1] = scanner.nextInt();
 		}
 		
-		for(int i = 0; i < num; i++) {
+		for(int i = 0; i < num; i++) { // 기준 행
 			int rank = 1;
-			for(int j = 0; j < num; j++) {
+			for(int j = 0; j < num; j++) { // 비교대상이 되는 행들
+				
 				if(i == j)
 					continue;
 				
 				if(array[i][0] < array[j][0] && array[i][1] < array[j][1])
 					rank++;
+				
+				
 			}
-			result[i] = rank;
+			resultArray[i] = rank;
 		}
 		
 		for(int i = 0; i < num; i++) {
-			System.out.print(result[i] + " ");
+			System.out.print(resultArray[i] + " ");
 		}
 		
 	}
