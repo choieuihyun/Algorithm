a609fee79d072900665fcd93bf87d6c717352344
choieuihyun <amammmaa1@naver.com>
2023-02-09 23:59:14 +0900

    test class

---
+++ AlgorithmStudy/src/study/Test.java
@@ -0,0 +1,43 @@
+package study;
+
+import java.util.Scanner;
+// 편안한 테스트의 현장
+public class Test {
+	
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt();
+		
+		int array[][] = new int[num][2];
+		int result[] = new int[num];
+		
+		
+		for(int i = 0; i < num; i++) {
+			array[i][0] = scanner.nextInt();
+			array[i][1] = scanner.nextInt();
+		}
+		
+		for(int i = 0; i < num; i++) {
+			int rank = 1;
+			for(int j = 0; j < num; j++) {
+				if(i == j)
+					continue;
+				
+				if(array[i][0] < array[j][0] && array[i][1] < array[j][1])
+					rank++;
+			}
+			result[i] = rank;
+		}
+		
+		for(int i = 0; i < num; i++) {
+			System.out.print(result[i] + " ");
+		}
+		
+		
+	}
+	
+	
+
+}
