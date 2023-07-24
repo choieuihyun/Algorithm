8e7637e541154e75e0d5c7a585d8f5cc9c1ea8ab
choeuihyeon <amammmaa1@naver.com>
2023-07-24 19:11:40 +0900

    크게 어렵진 않았지만 if문에서 조건의 순서에 따라서 에러가 날 수 있다는 것을 잊으면 안됨.

---
+++ AlgorithmStudy/src/stack/Q4949.java
@@ -0,0 +1,66 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q4949 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+	
+		while(true) {
+			
+			String s = scanner.nextLine();
+
+			if(s.equals("."))
+				break;
+			
+			System.out.println(stack(s));
+			
+		}
+		
+	}
+	
+	static String stack(String s) {
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int index = 0; index < s.length(); index++) {
+			
+			char c = s.charAt(index);
+			
+			if (c == '(' || c == '[') {
+				stack.push(c);
+			}
+			
+			else if (c == ')') {
+				
+				if ( stack.peek() != '(' || stack.empty()) {
+					return "no";
+				} else
+					stack.pop();
+				
+			}
+			
+			else if (c == ']') {
+				
+				if (stack.empty() || stack.peek() != '[') {
+					return "no";
+				} else
+					stack.pop();
+				
+			}
+			
+		}
+		
+		if (stack.empty())
+			return "yes";
+		else
+			return "no";
+		
+		//System.out.print(stack);
+		
+	}
+
+}
