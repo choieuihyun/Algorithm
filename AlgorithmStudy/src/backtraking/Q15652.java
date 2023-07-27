0e577dca5d83d84d0ebb85b9f6287ae483e74f83
choeuihyeon <amammmaa1@naver.com>
2023-07-27 16:21:25 +0900

    문제에서 가장 큰 힌트는 [1,1], [2,2], [3,3], [4,4]이었다. 같은 수로 시작하기에 index가 증가하면서 dfs의 파라미터로 들어가야한다는 의미로 해석함.

---
+++ AlgorithmStudy/src/backtraking/Q15652.java
@@ -0,0 +1,46 @@
+package backtraking;
+
+import java.util.Scanner;
+
+public class Q15652 {
+	
+	static int N,M;
+	static int [] array;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		array = new int[M];
+		
+		dfs(1, 0);
+		
+		System.out.print(sb);
+		
+	}
+	
+	static void dfs(int a, int depth) {
+	
+		if (depth == M) {
+			for(int val : array) {
+				sb.append(val + " ");
+			}
+			
+			sb.append('\n');
+			return;
+		}
+		
+		for(int index = a; index < N+1; index++) {
+			
+			array[depth] = index;
+			dfs(index, depth+1);
+			
+		}
+		
+	}
+
+}
