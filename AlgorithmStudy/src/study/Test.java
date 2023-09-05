c71eb4c15b0e7920bdf93be8635d3da51c048756
choeuihyeon <amammmaa1@naver.com>
2023-09-05 21:41:55 +0900

    퀸문제 테스트 해본건데 코드를 거의 외워버렸

--- AlgorithmStudy/src/study/Test.java
+++ AlgorithmStudy/src/study/Test.java
@@ -1,43 +1,60 @@
 package study;
 
 import java.util.Scanner;
-// 편안한 테스트의 현장
-public class Test {
-	
-	public static void main(String[] args) {
-	
-		Scanner scanner = new Scanner(System.in);
-		
-		int num = scanner.nextInt();
-		
-		int array[][] = new int[num][2];
-		int result[] = new int[num];
-		
-		
-		for(int i = 0; i < num; i++) {
-			array[i][0] = scanner.nextInt();
-			array[i][1] = scanner.nextInt();
-		}
-		
-		for(int i = 0; i < num; i++) {
-			int rank = 1;
-			for(int j = 0; j < num; j++) {
-				if(i == j)
-					continue;
-				
-				if(array[i][0] < array[j][0] && array[i][1] < array[j][1])
-					rank++;
-			}
-			result[i] = rank;
-		}
-		
-		for(int i = 0; i < num; i++) {
-			System.out.print(result[i] + " ");
-		}
-		
-		
-	}
-	
-	
 
+public class Test {
+ 
+	public static int[] arr;
+	public static int N;
+	public static int count = 0;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		N = in.nextInt();
+		arr = new int[N];
+ 
+		nQueen(0);
+		System.out.println(count);
+ 
+	}
+ 
+	public static void nQueen(int depth) {
+		// 모든 원소를 다 채운 상태면 count 증가 및 return 
+		// 행을 다 채우면 카운트를 1 증가시키고 리턴 
+		if (depth == N) {
+			count++;
+			return;
+		}
+ 
+		for (int i = 0; i < N; i++) {
+			arr[depth] = i;
+			// 놓을 수 있는 위치일 경우 재귀호출
+			// 해당 열에서 i 번째 행에 놓을 수 있는지를 검사하는 함수 
+			if (Possibility(depth)) {
+				nQueen(depth + 1);
+			}
+		}
+ 
+	}
+ 
+	public static boolean Possibility(int col) {
+ 
+		for (int i = 0; i < col; i++) {
+			// 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우) 
+			if (arr[col] == arr[i]) {
+				return false;
+			} 
+			
+			/*
+			 * 대각선상에 놓여있는 경우
+			 * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
+			 */
+			else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
