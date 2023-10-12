ee6ddeda16bd2ab2d65ad3f1cc8ac30ffc14fe1c
choeuihyeon <amammmaa1@naver.com>
2023-10-12 18:33:52 +0900

    recur -> for 주석 추가

--- AlgorithmStudy/src/dynamicProgramming/Q1904byFor.java
+++ AlgorithmStudy/src/dynamicProgramming/Q1904byFor.java
@@ -17,6 +17,7 @@ public class Q1904byFor {
 	
 	static int binary(int n) {
 		
+		// baseCondition은 항상 생각해야함.
 		if(n == 0) 
 			return 0;
 		
@@ -25,7 +26,11 @@ public class Q1904byFor {
 		
 		if(n == 2)
 			return 2;
-		
+	
+		// 바꿀 때 생각 과정 
+		// 1 2 3 5 8 13 21 34 이렇게 가는 수열에 가장 필요한게 뭐냐?
+		// 두개의 합, 앞의 수를 뒤의 수로 변경, 뒤의 수를 합으로 변경.
+		// 이걸 생각 후에 코드를 구현하면 12523401230배 편함.
 		int sum = 0;
 		int a = 1;
 		int b = 2;
