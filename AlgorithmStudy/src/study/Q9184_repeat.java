68838f395e915da4979b37eab066dadbb6c707ad
choeuihyeon <amammmaa1@naver.com>
2023-10-14 23:09:04 +0900

    자괴감 max

--- AlgorithmStudy/src/study/Q9184_repeat.java
+++ AlgorithmStudy/src/study/Q9184_repeat.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Q9184_repeat {
 	
-	static int [][][] wMemo = new int [21][21][21];
+	static int [][][] wMemo = new int [21][21][21]; // 아니 이건 경험부족일까 그냥 실력부족일까 a,b,c 세개의 변수를 가지고 계산하는건데 저장도 3개를 해야지
+													// 그러면 3차원 배열 써야지.
 
 	public static void main(String[] args) {
 		
@@ -28,15 +29,15 @@ public class Q9184_repeat {
 	
 	static int w(int a, int b, int c) {
 		
-		if (0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20 && wMemo[a][b][c] != 0) {
+		if (0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20 && wMemo[a][b][c] != 0) { // 최대 범위는 정해져 있기에 사용함.
 			return wMemo[a][b][c];
 		}
 		
-		if ( a <= 0 || b <= 0 || c <= 0 ) {
+		if ( a <= 0 || b <= 0 || c <= 0 ) { // baseCondition
 			return 1;
 		}
 		
-		else if ( a > 20 || b > 20 || c > 20 ) {
+		else if ( a > 20 || b > 20 || c > 20 ) { // baseCondition
 			return wMemo[20][20][20] = w(20, 20, 20);
 		}
 		
