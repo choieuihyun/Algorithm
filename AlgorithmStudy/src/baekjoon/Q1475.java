6167bc21f47d39aeb3960446e951f7f68b50cdb7
choieuihyun <amammmaa1@gmail.com>
2025-09-02 22:52:44 +0900

    백준 2025-09-02

--- AlgorithmStudy/src/baekjoon/Q1475.java
+++ AlgorithmStudy/src/baekjoon/Q1475.java
@@ -12,5 +12,19 @@ public class Q1475 {
             int num = s.charAt(i) - '0';
             count[num]++;
         }
+
+        int sixOrNine = (count[6] + count[9] + 1) / 2;
+
+        int max = 0;
+        for (int i = 0; i <= 9; i++) {
+            if (i == 6 || i == 9) {
+                continue;
+            }
+            max = Math.max(max, count[i]);
+        }
+
+        max = Math.max(max, sixOrNine);
+
+        System.out.println(max);
     }
 }
