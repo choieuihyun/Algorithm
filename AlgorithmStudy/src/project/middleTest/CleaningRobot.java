d51af9ab24a119fdedfaae3f8e3e6fc3a04bfca0
HyunE <amammmaa1@naver.com>
2026-09-21 18:48:07 +0900

    시뮬레이션 연습

---
+++ AlgorithmStudy/src/project/middleTest/CleaningRobot.java
@@ -0,0 +1,37 @@
+package project.middleTest;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CleaningRobot {
+    
+    static int N;
+    static int M;
+    static int[][] map;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        
+        
+
+    }
+}
